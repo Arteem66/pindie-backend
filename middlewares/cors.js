0208cf376bb403c6f3badb57a6c75696653ad3f8
SaldNevo <119350210+Arteem66@users.noreply.github.com>
2024-05-22 11:06:29 +0300

    change headers

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,7 @@
 const allowedCors = [
 	'https://pindiefrontsaldnevo.nomoredomainswork.ru',
 	'localhost:3000',
+	'localhost:3001',
 	'localhost:27017/pindie'
 ]
 
@@ -9,6 +10,11 @@ function cors(req, res, next) {
 
 	if (allowedCors.includes(origin)) {
 		res.header('Access-Control-Allow-Origin', origin)
+		res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
+		res.header(
+			'Access-Control-Allow-Headers',
+			'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization'
+		)
 	}
 
 	next()
