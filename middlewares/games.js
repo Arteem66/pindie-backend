998a77d82998fc498d86241e8e43d6a344c21860
SaldNevo <119350210+Arteem66@users.noreply.github.com>
2024-05-23 15:40:16 +0300

    update file

--- middlewares/games.js
+++ middlewares/games.js
@@ -76,21 +76,21 @@ const checkEmptyFields = async (req, res, next) => {
 		next()
 		return
 	}
-	// if (
-	// 	!req.body.title ||
-	// 	!req.body.description ||
-	// 	!req.body.image ||
-	// 	!req.body.link ||
-	// 	!req.body.developer
-	// ) {
-	// 	// Если какое-то из полей отсутствует, то не будем обрабатывать запрос дальше,
-	// 	// а ответим кодом 400 — данные неверны.
-	// 	res.setHeader('Content-Type', 'application/json')
-	// 	res.status(400).send(JSON.stringify({ message: 'Заполни все поля' }))
-	// } else {
-	// 	// Если всё в порядке, то передадим управление следующим миддлварам
-	// 	next()
-	// }
+	if (
+		!req.body.title ||
+		!req.body.description ||
+		!req.body.image ||
+		!req.body.link ||
+		!req.body.developer
+	) {
+		// Если какое-то из полей отсутствует, то не будем обрабатывать запрос дальше,
+		// а ответим кодом 400 — данные неверны.
+		res.setHeader('Content-Type', 'application/json')
+		res.status(400).send(JSON.stringify({ message: 'Заполни все поля' }))
+	} else {
+		// Если всё в порядке, то передадим управление следующим миддлварам
+		next()
+	}
 }
 
 // Файл middlewares/games.js
@@ -101,12 +101,12 @@ const checkIfCategoriesAvaliable = async (req, res, next) => {
 		next()
 		return
 	}
-  // if (!req.body.categories || req.body.categories.length === 0) {
-  //   res.setHeader("Content-Type", "application/json");
-  //       res.status(400).send(JSON.stringify({ message: "Выбери хотя бы одну категорию" }));
-  // } else {
-  //   next();
-  // }
+  if (!req.body.categories || req.body.categories.length === 0) {
+    res.setHeader("Content-Type", "application/json");
+        res.status(400).send(JSON.stringify({ message: "Выбери хотя бы одну категорию" }));
+  } else {
+    next();
+  }
 };
 
 // Файл middlewares/games.js
